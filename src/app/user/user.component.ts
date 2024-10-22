71ccb46145cf9722041933fcf659465004d74e2e
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-22 12:49:56 +0400

    :white_check_mark: Introducing signal. before ng version of 16 it was using Zone.js to detect element events and after 16 now we can use signal and it is the easy and efficient way done-28

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { DUMMY_USERS } from "../dummy-users";
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -12,17 +12,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
   // If we add private infront of this variable it will be available only for the class. Default is Public
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // When we use signals we dont need to create getters like below 'get imagePath' and directcly we can use computed function for that
+  // We use computed because this will be call only either of values inside being called or change only.
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
 
   // Instead of adding this complex binding html element itself, we can use getters here to make it and get it from there.
   // Because of getter we don't use parenthesis when we call this
-  get imagePath() {
+  /*get imagePath() {
     return 'assets/users/'+this.selectedUser.avatar;
-  }
+  }*/
 
   // Because of we call to this function we need to add parenthesis when we use it in HTML template
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
