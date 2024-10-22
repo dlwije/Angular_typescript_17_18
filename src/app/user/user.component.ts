1bed162a2811b5219b6880d51faa1d58314d9af1
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-22 09:31:32 +0400

    Using getters for computed values done-24

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,4 +13,10 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 export class UserComponent {
   // If we add private infront of this variable it will be available only for the class. Default is Public
   selectedUser = DUMMY_USERS[randomIndex];
+
+  // Instead of adding this complex binding html element itself, we can use getters here to make it and get it from there.
+  // Because of getter we don't use parenthesis when we call this
+  get imagePath() {
+    return 'assets/users/'+this.selectedUser.avatar;
+  }
 }
