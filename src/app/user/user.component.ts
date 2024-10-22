43a8679f6f01d8e62c0c8acd3950af8d1c9cf901
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-22 14:10:11 +0400

    :white_check_mark: Using signals for input variables in user.component.ts done-32

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, input, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,15 +9,24 @@ import {Component, Input} from '@angular/core';
 })
 export class UserComponent {
   // Here we had to add variable type as well because this introduced by us. Earlier we were using Angular inbuild functions and attributes.
-  // If we need to create components input we use below way.
+  // If we need to create components input, properties and custom inputs we use below way.
   // By adding '!' mark we tell that we have set value outside of this file to the variable.
-  @Input() avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
   @Input() id!: string;
 
-  get imagePath() {
+  // We can do the same input creation with signals in below way
+  // These are readonly variables only to this component but from app.component we can change
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
+
+  /*get imagePath() {
     return 'assets/users/' + this.avatar;
-  }
+  }*/
 
   onSelectUser(){}
 }
