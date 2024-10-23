626f45ebb1c597aae6303b5f54fab488b6ed981e
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-24 00:11:58 +0400

    :white_check_mark: Using Legacy Angular ngFor and ngIf done-43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgFor, NgIf} from '@angular/common';
 
 import { HeaderComponent } from "./header/header.component";
 import {UserComponent} from "./user/user.component";
@@ -9,7 +10,7 @@ import {TaskComponent} from "./task/task.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
