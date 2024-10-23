22124d596e4a12757abf2a7e365c1d0d55b95cb9
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-23 23:39:24 +0400

    :white_check_mark: Adding object as an @Input done-39

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,20 +11,27 @@ export class UserComponent {
   // Here we had to add variable type as well because this introduced by us. Earlier we were using Angular inbuild functions and attributes.
   // If we need to create components input, properties and custom inputs we use below way.
   // By adding '!' mark we tell that we have set value outside of this file to the variable.
-  @Input({ required: true }) avatar!: string;
+  /*@Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Input({ required: true }) id!: string;
+  @Input({ required: true }) id!: string;*/
+
+  // Instead of creating variable wise we can create it as an Object like below
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  }
 
   // Unlike input property output property will receive a default value.
   // This EventEmitter will allow us to Emit custom values to any parent components through this select property.
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser(){
     // We can use this value in parent components by calling to '$event' special variable
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
