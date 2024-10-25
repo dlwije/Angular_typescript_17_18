1ad1e6d31a827f1d8112c179ed30673dda725601
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-25 23:35:25 +0400

    :white_check_mark: save in localstorage - done 61

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,14 @@ export class TasksService {
     },
   ]
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks (userId: string) {
     return this.tasks.filter((task) => task.userId === userId)
   }
@@ -42,9 +50,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTask();
   }
 
   finishTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTask();
+  }
+
+  private saveTask () {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
