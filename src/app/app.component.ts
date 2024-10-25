cfe19a3e8f31c3e9aba73b92c6d026195220b486
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-26 00:56:50 +0400

    :white_check_mark: starting app with angular modules - done 64 to 67

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from "./header/header.component";
-import {UserComponent} from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
-import {TasksComponent} from "./tasks/tasks.component";
-import {TaskComponent} from "./tasks/task/task.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, TaskComponent],
+  // We had to remove below one for module based one
+  // standalone: true,
+  // imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
