2b8c36318bf97461616e6ac653bdb9d75bb0add4
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-25 15:28:50 +0400

    :white_check_mark: Transforming template data with pipes and here I used date pipe - done 57

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -3,12 +3,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 // It is optional to add 'type' keyword in front of interface name we imported here
 import { type Task } from "./task.model";
 import {CardComponent} from "../../shared/card/card.component";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [
-    CardComponent
+    CardComponent,
+    DatePipe
   ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
