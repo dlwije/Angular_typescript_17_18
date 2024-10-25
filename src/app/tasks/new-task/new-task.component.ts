586a6163577c63ac49263b6c1768077e8b956127
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-25 14:27:54 +0400

    Handling form submitting and store and create a new task done 55,54

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Output, signal} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {NewTaskData} from "../task/task.model";
 
 @Component({
   selector: 'app-new-task',
@@ -12,6 +13,7 @@ import {FormsModule} from "@angular/forms";
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
@@ -19,4 +21,13 @@ export class NewTaskComponent {
   onCancel () {
     this.cancel.emit();
   }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
+
 }
